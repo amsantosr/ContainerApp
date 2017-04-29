a115983b7965e8baef549227c793b8ab56882860
Abraham Santos <amsantosr@gmail.com>
2017-04-29 10:21:20 -0500

    Minor changes made

--- containersolution.h
+++ containersolution.h
@@ -11,21 +11,21 @@ class ContainerSolution : public QObject
 
 public:
     ContainerSolution(QObject *parent = 0);
-    void clear();
     void setContainerProblem(ContainerProblem *problem);
     const ContainerProblem *getContainerProblem() const { return containerProblem; }
     ContainerProblem *getContainerProblem() { return containerProblem; }
+    void setBoxes(QVector<int> boxLengthsX, QVector<int> boxLengthsY, QVector<int> boxLengthsZ,
+                  QVector<int> boxCoordinatesX, QVector<int> boxCoordinatesY, QVector<int> boxCoordinatesZ,
+                  QVector<int> packedBoxesGroups);
+    void clear();
     int boxLengthX(int index) const { return boxLengthXValues[index]; }
     int boxLengthY(int index) const { return boxLengthYValues[index]; }
     int boxLengthZ(int index) const { return boxLengthZValues[index]; }
     int boxCoordinateX(int index) const { return boxCoordinateXValues[index]; }
     int boxCoordinateY(int index) const { return boxCoordinateYValues[index]; }
     int boxCoordinateZ(int index) const { return boxCoordinateZValues[index]; }
-    void setBoxes(QVector<int> boxLengthsX, QVector<int> boxLengthsY, QVector<int> boxLengthsZ,
-                        QVector<int> boxCoordinatesX, QVector<int> boxCoordinatesY, QVector<int> boxCoordinatesZ,
-                        QVector<int> packedBoxesGroups);
-    int boxesCount() const { return packedBoxesGroupsIndexes.size(); }
     int boxGroupIndex(int i) const { return packedBoxesGroupsIndexes[i]; }
+    int boxesCount() const { return packedBoxesGroupsIndexes.size(); }
 
 signals:
     void beforeDataChange();
